5830c4614ec1fd8d38f1b9bf398bf26cedefc08f
RahulChauhan44 <92219110+RahulChauhan44@users.noreply.github.com>
2021-10-09 20:11:17 +0530

    Add files via upload

---
+++ src/main/java/com/incapp/controller/UserController.java
@@ -0,0 +1,144 @@
+package com.incapp.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.incapp.ReCaptchaResponse;
+import com.incapp.beans.User;
+
+@Controller
+public class UserController {
+	
+	final static private String URL="http://localhost:8889/";
+	RestTemplate restTemplate=new RestTemplate();
+	
+	
+	@Value("${google.recaptcha.verification.url}")
+	String recaptchaUrl;
+	@Value("${google.recaptcha.secret}")
+	String recaptchaSecret;
+	
+	@PostMapping("/UserRegister")
+	public String UserRegister(@ModelAttribute User u,ModelMap m, @RequestParam(name="g-recaptcha-response") String captchaResponse) {
+		
+		recaptchaSecret="?secret="+recaptchaSecret+"&response="+captchaResponse;
+		ReCaptchaResponse reCaptchaResponse=restTemplate.exchange(recaptchaUrl+recaptchaSecret, HttpMethod.POST,null,ReCaptchaResponse.class).getBody();
+		System.out.println(reCaptchaResponse.getChallenge_ts());
+		System.out.println(reCaptchaResponse.getHostName());
+		if(reCaptchaResponse.isSuccess()) {
+			String API="/userRegister";
+			HttpHeaders h=new HttpHeaders();
+			HttpEntity<User> requestEntity=new HttpEntity(u,h);
+			ResponseEntity<String> result=restTemplate.postForEntity(URL+API,requestEntity,String.class);
+			m.addAttribute("result", result.getBody());
+			return "index";
+		}else {
+			m.addAttribute("result", "Please Verify Captcha!");
+			return "index";
+		}
+	}
+	
+	@PostMapping("/UserLogin")
+	public String userLogin(String email,String password,ModelMap m,HttpSession session) {
+		
+		String API="/userLogin"+"/" + email+"/"+password;
+		ResponseEntity<String[]> result=restTemplate.getForEntity(URL+API,String[].class);
+		String r[]=result.getBody();
+		if(r.length==0) {
+			m.addAttribute("result", "Invalid Credentials!");
+			return "index";
+		}else {
+			session.setAttribute("userEmail", r[0]);
+			session.setAttribute("userName", r[1]);
+			
+			return "UserProfile";
+		}
+	}
+	
+	@RequestMapping("/UserPolicies")
+	public String userHome(ModelMap m,HttpSession session) {
+		String email=(String)session.getAttribute("userEmail");
+		if(email==null) {
+			return "index";
+		}
+		return "UserPolicies";
+	}
+	
+	@RequestMapping("/UserProfile")
+	public String UserProfile(ModelMap m,HttpSession session) {
+		String email=(String)session.getAttribute("userEmail");
+		if(email==null) {
+			return "index";
+		}
+		return "UserProfile";
+	}
+	
+	// Image upload code
+		@PostMapping("/UserImageUpload")
+		public String UserImageUpload(@RequestPart("photo") MultipartFile image,HttpSession session,ModelMap m) {
+			String email=(String)session.getAttribute("userEmail");
+			
+			String API="/userPhotoUpload"+"/" + email;
+			HttpHeaders h=new HttpHeaders();
+			h.setContentType(MediaType.MULTIPART_FORM_DATA);
+			
+			LinkedMultiValueMap<String, Object> data=new LinkedMultiValueMap<>();
+			
+			data.add("photo", convert(image));
+			
+			HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity=new HttpEntity(data,h);
+			
+			
+			ResponseEntity<String> result=restTemplate.postForEntity(URL+API,requestEntity,String.class);
+			m.addAttribute("result", result.getBody());
+			return "UserProfile";
+		}
+		
+		public static FileSystemResource convert(MultipartFile file) {
+			File convFile=new File(file.getOriginalFilename());
+			try {
+				convFile.createNewFile();
+				FileOutputStream fos=new FileOutputStream(convFile);
+				fos.write(file.getBytes());
+				fos.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+			return new FileSystemResource(convFile);
+		}
+		//END: Image upload code
+		
+		@RequestMapping("/getUserImage")
+		public void getUserImage(String email,HttpServletResponse response) {
+			try {
+				String API="/getUserImage"+"/" + email;
+				byte[] b=restTemplate.getForObject(URL+API,byte[].class);
+				response.getOutputStream().write(b);
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+}
